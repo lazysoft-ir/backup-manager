3771d0aa2d22a16bdeab01a10a813986aedf9e7b
Ggprox <76654246+Ggprox@users.noreply.github.com>
2021-04-26 16:15:58 +0430

    Update zip.py

--- zip.py
+++ zip.py
@@ -1,5 +1,22 @@
+import sqlite3
+from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog
 from zipfile import ZipFile
-import os
-# // just put the name and location file
-os.chdir(r'location file')
-ZipFile('archive.zip', 'w').write('your file name with type')
+
+root=tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+file = str(file_path)
+ZipFile('archive.zip', 'w').write(file)
+
+time = datetime.now().strftime("%B %d, %Y %I:%M%p")
+connection = sqlite3.connect('DataBase.db')
+cursor =connection.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS uploder (ID INTEGER PRIMARY KEY AUTOINCREMENT , datetime date ,filename VARCHAR );''')
+cursor.execute("INSERT INTO uploder (datetime , filename) VALUES (?,?);",(time,file))
+
+
+connection.commit()
+connection.close()
